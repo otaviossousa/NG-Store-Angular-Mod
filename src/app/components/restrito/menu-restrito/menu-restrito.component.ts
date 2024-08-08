72eb54f5ac08c6ee7cbd6d25953661c1135e47dc
Fernanda-Farias <contato.ffarias@gmail.com>
2024-08-08 00:46:59 -0300

    new menu restrito

--- src/app/components/restrito/menu-restrito/menu-restrito.component.ts
+++ src/app/components/restrito/menu-restrito/menu-restrito.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoginService } from 'src/app/login.service';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @Component({
   selector: 'app-menu-restrito',
